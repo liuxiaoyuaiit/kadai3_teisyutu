9acc0da2ea2862d5efa3a09ac68e0b15f62569a2
17745149 <chiren0808@yahoo.co.jp>
2017-05-31 21:26:39 +0900

    testandtest

--- 17745149_chk.sh
+++ 17745149_chk.sh
@@ -11,9 +11,10 @@ ERROR_EXIT () {
 #TEST#
 ######
 echo aaa 120 NG > $tmp-ans
-./17745149.sh aaa 120 > $tmp-out || ERROR_EXIT "TEST-1"
+./17745149.sh aaa 120 > $tmp-out && ERROR_EXIT "TEST-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+diff $tmp-ans $tmp-out && echo "TEST OK"
 
-echo OK
+echo ALL OK
 rm -f $tmp-*
 exit 0
