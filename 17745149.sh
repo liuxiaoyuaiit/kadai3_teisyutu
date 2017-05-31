d9738684a2085f5dbc45c2be6fe7d77695468153
17745149 <chiren0808@yahoo.co.jp>
2017-05-31 20:03:20 +0900

    first commit

---
+++ 17745149.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#最大公約数#
+#./17745149 a b で実行します#
+
+a=$1
+b=$2
+
+if [ $a = $b ];
+	then
+		echo $b
+	else
+		while [ 0 -lt $a ]
+	do
+		r=`expr $b % $a`
+		b=$a
+		a=$r
+	done
+	echo $b
+fi
