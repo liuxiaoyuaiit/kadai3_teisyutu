4ab81740353adfd21dc73d3b4d2df6d66c400131
17745149 <chiren0808@yahoo.co.jp>
2017-06-09 20:50:40 +0900

    goshitekisyusei4

--- 17745149_chk.sh
+++ 17745149_chk.sh
@@ -10,7 +10,7 @@ ERROR_EXIT () {
 ######
 #TEST#
 ######
-echo aaa 120 NG > $tmp-ans
+echo 120 > $tmp-ans
 ./17745149.sh 100 120 > $tmp-out && ERROR_EXIT "TEST1-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 ##diff $tmp-ans $tmp-out && echo "TEST OK"
